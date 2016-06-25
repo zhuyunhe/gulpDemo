7515d70d0a435c230cee8cad3941f0decd820914
unknown <356759823@qq.com>
2016-06-25 17:44:56 +0800

    gulp

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # gulpDemo
 用Gulp构建一个前端小工程
 用于分享和学习
+
+## 用到的Gulp插件
+### JavaScript相关
+1. js文件压缩丑化：[gulp-uglify](https://www.npmjs.com/package/gulp-uglify/)
+2. js代码检查：[gulp-jshint](https://www.npmjs.com/package/gulp-jshint/)
+
+### CSS相关
+1. css文件压缩：[gulp-clean-css](https://www.npmjs.com/package/gulp-clean-css/)
+
+### html相关
+1. html文件压缩：[gulp-htmlmin](https://www.npmjs.com/package/gulp-htmlmin/)
+
+### 图片相关
+1. 图片压缩：[gulp-imagemin](https://www.npmjs.com/package/gulp-imagemin/)
+
+### 文件相关
+1. 重命名文件：[gulp-rename](https://www.npmjs.com/package/gulp-rename/)
+2. 合并文件：[gulp-concat](https://www.npmjs.com/package/gulp-concat/)
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,52 @@
 var gulp = require('gulp');
+var cleancss = require('gulp-cleancss');
+var htmlmin = require('gulp-htmlmin');
+var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
+var jshint = require('gulp-jshint');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+
 gulp.task('default',function(){
-console.log('hello gulp');
+	console.log('hello gulp');
 });
 
-gulp.task('one',function(){
-	setTimeout(function(){
-		console.log('one is done');
-	},5000);
-});
-gulp.task('two', ['one'], function(){
-	console.log('two is done');
-});
+//html处理
+gulp.task('html',function(){
+	var src = './src/*.html';
+	var dest = './dist/';
 
-gulp.task('modifyJS',function(){
-	console.log('修改了JS文件');
+	gulp.src(src)
+		.pipe(htmlmin())
+		.pipe(gulp.dest(dest));
 });
 
-gulp.watch('3.5/js/*.js',['modifyJS']);
+//js处理
+gulp.task('js',function(){
+	var src = './src/js/*.js';
+	var dest = './dist/js';
 
-gulp.watch('3.5/js/*.js', function(){
-	console.log('modify JS');
+	gulp.src(src)
+		.pipe(jshint())
+		.pipe(jshint.reporter('zyh-JS-report'))
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest(dest))
+		.pipe(rename({suffix:'.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest(dest));
 });
 
-gulp.task();
+//css处理
+gulp.task('css',function(){
+	var src = './src/css/*.css';
+	var dest = './dist/css';
+
+	gulp.src(src)
+		.pipe(concat('main.css'))
+		.pipe(gulp.dest(dest))
+		.pipe(rename({suffix:'.min'}))
+		.pipe(cleancss())
+		.pipe(gulp.dest(dest));
+});
+
+//图片处理
